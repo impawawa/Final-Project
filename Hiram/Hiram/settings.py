a98eae94ad12024fc67f26ba22a7b96e013118d2
impawawa <152131860+impawawa@users.noreply.github.com>
2025-05-31 03:57:23 +0800

    ...

--- Hiram/Hiram/settings.py
+++ Hiram/Hiram/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Hiram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hiram_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',  # Change this to your actual PostgreSQL password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
